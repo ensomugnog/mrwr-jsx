418aba4827155750cd78065ffec4314788b5510a
Enso Mugno Galvis <ensomugnog@gmail.com>
2026-01-29 05:04:05 +1000

    04 - Shorthand JS Expressions

--- src/index.js
+++ src/index.js
@@ -9,13 +9,8 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4) Create a component
-function App() {
-    let message = 'Bye there!';
-    if (Math.random() > 0.5) {
-        message = 'Hello there!';
-    }
-    
-    return <h1>{message}</h1>;
+function App() {  
+    return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 
 // 5) Show the component on the screen 
